ca8c63e1fcffdb61d43ab8f583509e6bf20f8626
Aizar Farhan <andiaizarfarhan@gmail.com>
2025-01-10 16:23:19 +0700

    update API

--- src/api/categories.ts
+++ src/api/categories.ts
@@ -1,16 +1,13 @@
-import axios from "axios";
+import axios from "../utils/axioxInstance";
 
-const BASE_URL = "https://api.escuelajs.co/api/v1";
-
-// Get all categories
+// untuk get all category
 export const getCategories = async () => {
-  const response = await axios.get(`${BASE_URL}/categories`);
-
-  console.log(response);
+  const response = await axios.get("/categories");
+  return response.data;
 };
 
-// Get products by category ID
+// untuk get product by category
 export const getProductsByCategory = async (categoryId: number) => {
-  const response = await axios.get(`${BASE_URL}/products/?categoryId=${categoryId}`);
+  const response = await axios.get(`/products/?categoryId=${categoryId}`);
   return response.data;
 };
--- src/api/products.ts
+++ src/api/products.ts
@@ -1,11 +1,12 @@
 import axios from "../utils/axioxInstance";
 
+// untuk get all product
 export const getProducts = async () => {
   const response = await axios.get("/products");
   return response.data;
-  console.log(response.data);
 };
 
+// untuk get product by category
 export const getProductByCategory = async (categoryId: number) => {
   const response = await axios.get(`/products/?categoryId=${categoryId}`);
   return response.data;
